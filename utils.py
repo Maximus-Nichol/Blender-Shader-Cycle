35b1aebafac8c9aa7ccd3f80d0fa6ac66d1a5bbd
Maximus-Nichol <nicholmax8@gmail.com>
2026-03-17 20:14:38 +0000

    add logic to get_colour_node

--- utils.py
+++ utils.py
@@ -1,6 +1,22 @@
 import bpy
 
 def get_node_colour(node):
-    """
-    Will get the colour value from a node.
-    """
+    # 1. Check for Principled BSDF
+    if "Base Colour" in node.inputs:
+        return node.inputs["Base Colour"].default_value[:]
+    
+    # 2. Check for 'Color' (Diffuse BSDF, Emission, etc.)
+    if "Colour" in node.inputs:
+        return node.inputs["Colour"].default_value[:]
+        
+    # 3. Fallback to viewport color
+    return (0.8, 0.8, 0.8, 1.0)
+
+def find_output_node(material):
+    # Finds material output
+    if not material.node_tree:
+        return None
+    for node in material.node_tree.nodes:
+        if node.type == 'OUTPUT_MATERIAL' and node.is_active_output:
+            return node
+    return None
