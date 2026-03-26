3f71614354cfaf7cd21167767e8585cddbebb544
Maximus-Nichol <nicholmax8@gmail.com>
2026-03-26 19:40:09 +0000

    Implement material cycling and restoration operators

--- operators.py
+++ operators.py
@@ -5,7 +5,45 @@ class OT_CycleTextures(bpy.types.Operator):
     bl_label = "Cycle Materials"
     
     def execute(self, context):
-        self.report({'INFO'}, "NOT IMPLEMENTED")
+        props = context.scene.texture_cycle_data
+        props.backup_data.clear() # Start fresh
+        
+        # Iterate over all materials in the file
+        for mat in bpy.data.materials:
+            # Filter: Only process materials being used and that use nodes
+            if mat.users > 0 and mat.use_nodes:
+                
+                output_node = utils.find_output_node(mat)
+                if not output_node:
+                    continue
+                
+                surface_input = output_node.inputs['Surface']
+                
+                # Check if something is actually plugged into the Surface
+                if surface_input.is_linked:
+                    original_link = surface_input.links[0]
+                    original_node = original_link.from_node
+                    
+                    # 1. Save state to our CollectionProperty
+                    item = props.backup_data.add()
+                    item.material_name = mat.name
+                    item.original_node_name = original_node.name
+                    
+                    # 2. Get the color using your utility
+                    extracted_color = utils.get_node_color(original_node)
+                    
+                    # 3. Create the Diagnostic Shader (Emission is great for this)
+                    nodes = mat.node_tree.nodes
+                    diag_node = nodes.new(type='ShaderNodeEmission')
+                    diag_node.name = "TC_DIAGNOSTIC"
+                    diag_node.label = "Diagnostic Mode"
+                    diag_node.inputs['Color'].default_value = extracted_color
+                    
+                    # 4. Re-link: New Node -> Material Output
+                    mat.node_tree.links.new(diag_node.outputs[0], surface_input)
+
+        props.is_cycled = True
+        self.report({'INFO'}, f"Cycled {len(props.backup_data)} materials.")
         return {'FINISHED'}
 
 class OT_RestoreTextures(bpy.types.Operator):
